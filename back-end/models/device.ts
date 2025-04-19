63641cd4e0ce127c857a67afb81eae3e308de3f8
Thomshot <thomasnguyen2207@gmail.com>
2025-04-19 11:04:09 +0200

    MAJ DBB objets

--- back-end/models/device.ts
+++ back-end/models/device.ts
@@ -1,12 +1,26 @@
 import mongoose from 'mongoose';
 
 const deviceSchema = new mongoose.Schema({
+  idUnique: { type: String, required: true, unique: true },
   nom: { type: String, required: true },
   type: { type: String, required: true },
   statutActuel: { type: String, required: true },
   états: { type: [String], required: true },
   fonctionnalités: { type: [String], required: true },
   dernièreMiseÀJour: { type: Date, default: Date.now },
+  températureActuelle: { type: Number },
+  températureCible: { type: Number },
+  mode: { type: String },
+  connectivité: { type: String },
+  étatDeBatterie: { type: String },
+  résolution: { type: String },
+  couleurActuelle: { type: String },
+  luminosité: { type: String },
+  consommationActuelle: { type: String },
+  niveauDeBatterie: { type: String },
+  humidité: { type: String },
+  volumeActuel: { type: String },
+  dernièreInteraction: { type: Date, default: Date.now }
 });
 
 export default mongoose.model('Device', deviceSchema);
