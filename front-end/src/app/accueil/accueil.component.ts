19fc50101aa2b99fd3abe0e843385feb3e57bb96
Matthieu Brault <matthieu92.brault@gmail.com>
2025-04-22 21:44:00 +0200

    reglage bug commit

--- front-end/src/app/accueil/accueil.component.ts
+++ front-end/src/app/accueil/accueil.component.ts
@@ -1,9 +1,7 @@
-<<<<<<< HEAD
-import { Component,inject } from '@angular/core';
+
 import { ViewChild } from '@angular/core';
-=======
 import { Component, OnInit, inject } from '@angular/core';
->>>>>>> 8b92d6da6fed73f7e092657da1ed62cbacbca4b9
+
 import { CommonModule } from '@angular/common';
 import { MaterialDModule } from '../shared/material-d.module';
 import { BreakpointObserver } from '@angular/cdk/layout';
@@ -12,28 +10,21 @@ import { Observable } from 'rxjs';
 import { map, startWith } from 'rxjs/operators';
 import { ProfilComponent } from '../profil/profil.component';
 import { HttpClient } from '@angular/common/http';
-<<<<<<< HEAD
 import { RouterModule } from '@angular/router';
 import {MatDialog} from '@angular/material/dialog';
 import { AjoutObjetDialogComponent } from './ajout-objet-dialog/ajout-objet-dialog.component';
 import { FiltreDialogComponent } from './filtre-dialog/filtre-dialog.component';
 import { ProgressionNiveauDialogComponent } from './progression-niveau-dialog/progression-niveau-dialog.component';
-=======
 import { Router } from '@angular/router';
->>>>>>> 8b92d6da6fed73f7e092657da1ed62cbacbca4b9
+
 
 @Component({
   selector: 'app-accueil',
   standalone: true,
-<<<<<<< HEAD
+
   imports: [MaterialDModule, CommonModule, ProfilComponent, FormsModule,RouterModule], // ✅ Add FormsModule here
   templateUrl: './accueil.component.html', // Ensure this path is correct
   styleUrls: ['./accueil.component.scss'] // Ensure this path is correct
-=======
-  imports: [MaterialDModule, CommonModule, ProfilComponent, FormsModule],
-  templateUrl: './accueil.component.html',
-  styleUrls: ['./accueil.component.scss']
->>>>>>> 8b92d6da6fed73f7e092657da1ed62cbacbca4b9
 })
 export class AccueilComponent implements OnInit {
 
@@ -50,8 +41,7 @@ export class AccueilComponent implements OnInit {
   serviceSearchResults: any[] = [];
   serviceSearchTriggered: boolean = false;
 
-<<<<<<< HEAD
-  constructor(private breakpointObserver: BreakpointObserver, private http: HttpClient) {
+  constructor(private breakpointObserver: BreakpointObserver, private http: HttpClient,private router: Router) {
   }
   readonly dialog = inject(MatDialog);
 
@@ -82,17 +72,10 @@ export class AccueilComponent implements OnInit {
     });
   }
   
-=======
+
   userSearchQuery: string = '';
   userSearchResults: any[] = [];
   userSearchTriggered: boolean = false;
-
-  constructor(
-    private breakpointObserver: BreakpointObserver,
-    private http: HttpClient,
-    private router: Router
-  ) {}
->>>>>>> 8b92d6da6fed73f7e092657da1ed62cbacbca4b9
 
   ngOnInit(): void {
     this.breakpointObserver.observe(['(max-width: 960px)']).subscribe(result => {
